f75e4dd936231e054e525800cbaf68645c69b955
Ryan <ryan@scalr.com>
2025-06-05 09:27:22 -0400

    Update s3.tf

--- tf_code/s3.tf
+++ tf_code/s3.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "example_bucket" {
 # Lifecycle configuration for the bucket - uses "bucket" attribute
 resource "aws_s3_bucket_lifecycle_configuration" "example_lifecycle" {
   # IMPORTANT: Using "bucket" attribute instead of "id" to reference the bucket
-  bucket = aws_s3_bucket.example_bucket.bucket
+  bucket = aws_s3_bucket.example_bucket.id
   
   rule {
     id     = "intelligent_tiering_rule"
