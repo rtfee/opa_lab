3da6a1f4f6709427b50e0c92004ac78eae5dc0df
Ryan <ryan@scalr.com>
2025-06-20 11:13:44 -0400

    Update s3.tf

--- tf_code/s3.tf
+++ tf_code/s3.tf
@@ -9,29 +9,3 @@ resource "aws_s3_bucket" "example_bucket" {
   }
 }
 
-# Lifecycle configuration for the bucket - uses "bucket" attribute
-resource "aws_s3_bucket_lifecycle_configuration" "example_lifecycle" {
-  # IMPORTANT: Using "bucket" attribute instead of "id" to reference the bucket
-  bucket = aws_s3_bucket.example_bucket.id
-  
-  rule {
-    id     = "intelligent_tiering_rule"
-    status = "Disabled"
-
-   filter {  
-      prefix = ""  
-    } 
-    
-    
-    # Optional: Additional transitions for cost optimization
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-    
-    transition {
-      days          = 90
-      storage_class = "GLACIER"
-    }
-  }
-}
