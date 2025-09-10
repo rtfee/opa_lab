42c0b21aea6ff1f60b42fd3fde682023a96b56ec
Ryan <ryan@scalr.com>
2025-09-10 11:30:37 -0400

    Update and rename label.tf to s3_from_registry.tf

--- tf_code/label.tf
+++
@@ -1,4 +0,0 @@
-module "label" {
-	source  = "scalrdemov2.scalr.io/acc-v0ohpf7km39ru2dtn/label/null"
-	version = "1.0.1"
-}
---
+++ tf_code/s3_from_registry.tf
@@ -0,0 +1,9 @@
+module "s3-bucket" {
+	source  = "scalrdemov2.scalr.io/acc-v0ohpf7km39ru2dtn/s3-bucket/aws"
+	version = "5.0.0"
+
+	# Set 1 required variable below.
+
+	# (Optional, Forces new resource) The name of the bucket. If omitted, Terraform will assign a random, unique name.
+ 	bucket = <value>
+}
